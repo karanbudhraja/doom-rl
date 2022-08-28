50fe6a94bc452a9445c8d5bf7bfac084a939fba6
karan <karanb1@umbc.edu>
2022-08-27 20:55:30 -0500

    prepare full run

--- run.py
+++ run.py
@@ -62,8 +62,8 @@ def main():
     # iteration
     #
 
-    iterations = 5
-    episodes_per_iteration = 3
+    iterations = 1000
+    episodes_per_iteration = 32
     sleep_time = 1.0 / vzd.DEFAULT_TICRATE
     iteration_average_loss_values = []
     iteration_average_total_reward_values = []
