665e63708b191a9d98b7c1861dfdfff3eab5525c
karan <karanb1@umbc.edu>
2022-09-16 00:00:52 -0500

    add value function

--- agents/networks.py
+++ agents/networks.py
@@ -1,5 +1,35 @@
 import torch.nn as nn
 
+class VNet(nn.Module):
+    # deep v learning architecture
+    def __init__(self, available_actions_count):
+        super().__init__()
+        self.convolution_1 = nn.Sequential(nn.Conv2d(1, 8, kernel_size=3, stride=2, bias=False),
+                                            nn.BatchNorm2d(8),
+                                            nn.ReLU())
+        self.convolution_2 = nn.Sequential(nn.Conv2d(8, 8, kernel_size=3, stride=2, bias=False),
+                                            nn.BatchNorm2d(8),
+                                            nn.ReLU())
+        self.convolution_3 = nn.Sequential(nn.Conv2d(8, 8, kernel_size=3, stride=1, bias=False),
+                                            nn.BatchNorm2d(8),
+                                            nn.ReLU())
+        self.convolution_4 = nn.Sequential(nn.Conv2d(8, 16, kernel_size=3, stride=1, bias=False),
+                                            nn.BatchNorm2d(16),
+                                            nn.ReLU())
+        self.linear_state_value = nn.Sequential(nn.Linear(192, 64),
+                                        nn.ReLU(),
+                                        nn.Linear(64, 1))
+
+    def forward(self, state):
+        state = self.convolution_1(state)
+        state = self.convolution_2(state)
+        state = self.convolution_3(state)
+        state = self.convolution_4(state)
+        state = state.view(-1, 192)
+        state_value = self.linear_state_value(state).reshape(-1, 1)
+
+        return state_value
+
 class QNet(nn.Module):
     # deep q learning architecture
     def __init__(self, available_actions_count):
